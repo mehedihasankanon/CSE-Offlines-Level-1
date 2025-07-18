a825ee48ba3046208a6852767b3f07919d63929c
Mehedi Hasan Kanon <mehedi.hkanon@gmail.com>
2025-07-18 20:13:18 +0600

    Add solutions, test files, and stress scripts for Offline 7
    Implemented solutions for two problems in 2305052_1.cpp and 2305052_2.cpp. Added input/output sample files and Python scripts (stress.py, stress2.py) for generating and validating test cases. Updated .gitignore to exclude CPH tester directories.

--- L-1 T-2/CSE106/Offline_7/2305052_1.cpp
+++ L-1 T-2/CSE106/Offline_7/2305052_1.cpp
@@ -4,4 +4,78 @@ using namespace std;
 
 using ll = long long;
 
+#define dbg(x) cout << __LINE__ << " " << #x << " " << x << endl
 
+int main()
+{
+
+    // if (!freopen("input.txt", "r", stdin))
+    // {
+    //     cerr << "Failed to open input.txt" << endl;
+    //     return 1;
+    // }
+    // if (!freopen("output.txt", "w", stdout))
+    // {
+    //     cerr << "Failed to open output.txt" << endl;
+    //     return 1;
+    // }
+
+    int n, k;
+    cin >> n >> k;
+
+    if (k > n)
+    {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    ll *load = new ll[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> load[i];
+    }
+
+    ll l = 0, r = 1e18;
+    while (r - l > 1)
+    {
+        ll mid = l + (r - l) / 2;
+
+        ll cnt = 1, curr_workload = 0;
+        int it = 0;
+        bool possible = true;
+        while (it < n)
+        {
+            if (load[it] > mid)
+            {
+                possible = !possible;
+                break;
+            }
+
+            if (curr_workload + load[it] <= mid)
+            {
+                curr_workload += load[it++];
+            }
+            else
+            {
+                cnt++;
+                curr_workload = load[it++];
+            }
+        }
+
+        // dbg(mid);
+        // dbg(cnt);
+
+        if (possible && cnt <= k)
+        {
+            r = mid;
+        }
+        else
+        {
+            l = mid;
+        }
+    }
+
+    cout << r << endl;
+
+    delete[] load;
+}
--- L-1 T-2/CSE106/Offline_7/2305052_2.cpp
+++ L-1 T-2/CSE106/Offline_7/2305052_2.cpp
@@ -2,6 +2,93 @@
 
 using namespace std;
 
-using ll = long long;
+void cleanup(int **benches, int n, int b);
 
+int main()
+{
+    // if (!freopen("input.txt","r",stdin)) {
+    //     cerr << "Failed to open input.txt" << endl;
+    //     return 1;
+    // }
+    // if (!freopen("output.txt","w",stdout)) {
+    //     cerr << "Failed to open output.txt" << endl;
+    //     return 1;
+    // }
 
+    int n, b;
+    cin >> n >> b;
+
+    int **benches = new int *[b];
+    for (int i = 0; i < b; i++)
+    {
+        benches[i] = new int[n / b];
+        for (int j = 0; j < n / b; j++)
+        {
+            cin >> benches[i][j];
+        }
+    }
+
+    for (int i = 0; i < b; i++)
+    {
+        for (int j = 0; j < n / b; j++)
+        {
+            if (benches[i][j] == 1 || benches[i][j] == 2)
+            {
+                if (j < n / b - 1)
+                {
+                    if (benches[i][j + 1] == 1 || benches[i][j + 1] == 2)
+                    {
+                        cout << "false" << endl;
+                        cleanup(benches, n, b);
+                        return 0;
+                    }
+                    benches[i][j + 1] = -1;
+                }
+                if (benches[i][j] == 2)
+                {
+                    if (j && (benches[i][j - 1] == 1 || benches[i][j - 1] == 2))
+                    {
+                        cout << "false" << endl;
+                        cleanup(benches, n, b);
+                        return 0;
+                    }
+                    benches[i][j - 1] = -1;
+                }
+            }
+        }
+    }
+
+    int cnt = 0;
+    for (int i = 0; i < b; i++)
+    {
+        for (int j = 0; j < n / b; j++)
+        {
+            // cout << benches[i][j] << " ";
+            cnt += (benches[i][j] == 0);
+        }
+        // cout << endl;
+    }
+
+    int m;
+    cin >> m;
+
+    if (cnt >= m)
+    {
+        cout << "true" << endl;
+    }
+    else
+        cout << "false" << endl;
+
+    cleanup(benches, n, b);
+    return 0;
+}
+
+void cleanup(int **benches, int n, int b)
+{
+
+    for (int i = 0; i < b; i++)
+    {
+        delete[] benches[i];
+    }
+    delete[] benches;
+}
